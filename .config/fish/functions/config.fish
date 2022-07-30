1be7180be1e7e7d40f2dde0554b5cf1d8531be1b
Esa Nuurtamo <esa.nuurtamo@helsinki.fi>
2022-07-30 15:27:49 +0200

    changed fish settings and added alias

---
+++ .config/fish/functions/config.fish
@@ -0,0 +1,3 @@
+function config --wraps='/usr/bin/git --git-dir=/Users/Esa/.cfg/ --work-tree=/Users/Esa' --description 'alias config=/usr/bin/git --git-dir=/Users/Esa/.cfg/ --work-tree=/Users/Esa'
+  /usr/bin/git --git-dir=/Users/Esa/.cfg/ --work-tree=/Users/Esa $argv; 
+end
